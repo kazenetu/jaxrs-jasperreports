e4379603b1de9ec68e0d99d765bfaa9279baa436
kazenetu <rakudafactory@gmail.com>
2017-02-19 10:26:54 +0900

    タブとスペース混在を改善。スペースに統一

--- jaxrs/src/main/java/com/github/kazenetu/jerseyServer/resource/DownloadResource.java
+++ jaxrs/src/main/java/com/github/kazenetu/jerseyServer/resource/DownloadResource.java
@@ -24,179 +24,179 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 @Path("dl")
 public class DownloadResource {
 
-	/**
-	 * PDF(Label表示)の表示
-	 * @return
-	 */
-	@GET
-	@Path("pdf")
-	@Produces("application/pdf")
-	public Response pdf() {
+    /**
+     * PDF(Label表示)の表示
+     * @return
+     */
+    @GET
+    @Path("pdf")
+    @Produces("application/pdf")
+    public Response pdf() {
 
-		List<?> dataSourceList = Arrays.asList(new TestData("name", 20));
-		JRDataSource dataSource = new JRBeanCollectionDataSource(dataSourceList);
+        List<?> dataSourceList = Arrays.asList(new TestData("name", 20));
+        JRDataSource dataSource = new JRBeanCollectionDataSource(dataSourceList);
 
-		Map<String, Object> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
 
-		try {
-			return Response.ok(getPdfBytes("sample", params, dataSource))
-					// .header("Content-Disposition", "attachment; filename=" +
-					// URLEncoder.encode("test.pdf", "utf-8"))
-					.build();
-		} catch (JRException e) {
-			return Response.serverError().build();
-		}
-	}
+        try {
+            return Response.ok(getPdfBytes("sample", params, dataSource))
+                    // .header("Content-Disposition", "attachment; filename=" +
+                    // URLEncoder.encode("test.pdf", "utf-8"))
+                    .build();
+        } catch (JRException e) {
+            return Response.serverError().build();
+        }
+    }
 
-	/**
-	 * PDF(バーコード表示)の表示
-	 * @return
-	 */
-	@GET
-	@Path("pdf2")
-	@Produces("application/pdf")
-	public Response pdf2() {
+    /**
+     * PDF(バーコード表示)の表示
+     * @return
+     */
+    @GET
+    @Path("pdf2")
+    @Produces("application/pdf")
+    public Response pdf2() {
 
-		List<?> dataSourceList = Arrays.asList(new TestData("name", 20));
-		JRDataSource dataSource = new JRBeanCollectionDataSource(dataSourceList);
+        List<?> dataSourceList = Arrays.asList(new TestData("name", 20));
+        JRDataSource dataSource = new JRBeanCollectionDataSource(dataSourceList);
 
-		Map<String, Object> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
 
-		try {
-			byte[] result = getPdfBytes("sample2", params, dataSource);
-			return Response.ok(result)
-					// .header("Content-Disposition", "attachment; filename=" +
-					// URLEncoder.encode("test.pdf", "utf-8"))
-					.build();
-		} catch (JRException e) {
-			return Response.serverError().build();
-		}
-	}
+        try {
+            byte[] result = getPdfBytes("sample2", params, dataSource);
+            return Response.ok(result)
+                    // .header("Content-Disposition", "attachment; filename=" +
+                    // URLEncoder.encode("test.pdf", "utf-8"))
+                    .build();
+        } catch (JRException e) {
+            return Response.serverError().build();
+        }
+    }
 
-	/**
-	 * PDF(日本語表示)の表示
-	 * @return
-	 */
-	@GET
-	@Path("pdf3")
-	@Produces("application/pdf")
-	public Response pdf3() {
+    /**
+     * PDF(日本語表示)の表示
+     * @return
+     */
+    @GET
+    @Path("pdf3")
+    @Produces("application/pdf")
+    public Response pdf3() {
 
-		List<?> dataSourceList = Arrays.asList(new TestData("name", 20));
-		JRDataSource dataSource = new JRBeanCollectionDataSource(dataSourceList);
+        List<?> dataSourceList = Arrays.asList(new TestData("name", 20));
+        JRDataSource dataSource = new JRBeanCollectionDataSource(dataSourceList);
 
-		Map<String, Object> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
 
-		try {
-			byte[] result = getPdfBytes("sample3", params, dataSource);
-			return Response.ok(result)
-					// .header("Content-Disposition", "attachment; filename=" +
-					// URLEncoder.encode("test.pdf", "utf-8"))
-					.build();
-		} catch (JRException e) {
-			return Response.serverError().build();
-		}
-	}
+        try {
+            byte[] result = getPdfBytes("sample3", params, dataSource);
+            return Response.ok(result)
+                    // .header("Content-Disposition", "attachment; filename=" +
+                    // URLEncoder.encode("test.pdf", "utf-8"))
+                    .build();
+        } catch (JRException e) {
+            return Response.serverError().build();
+        }
+    }
 
-	/**
-	 * PDF(オブジェクト関連付け)の表示
-	 * @return
-	 */
-	@GET
-	@Path("pdf4")
-	@Produces("application/pdf")
-	public Response pdf4() {
+    /**
+     * PDF(オブジェクト関連付け)の表示
+     * @return
+     */
+    @GET
+    @Path("pdf4")
+    @Produces("application/pdf")
+    public Response pdf4() {
 
-		List<TestData> src = new ArrayList<>();
-		src.add(new TestData("name20", 20));
-		src.add(new TestData("name21", 21));
-		src.add(new TestData("name22", 22));
-		src.add(new TestData("name23", 23));
-		src.add(new TestData("name24", 24));
-		src.add(new TestData("name25", 25));
-		//List<?> dataSourceList = src;
+        List<TestData> src = new ArrayList<>();
+        src.add(new TestData("name20", 20));
+        src.add(new TestData("name21", 21));
+        src.add(new TestData("name22", 22));
+        src.add(new TestData("name23", 23));
+        src.add(new TestData("name24", 24));
+        src.add(new TestData("name25", 25));
+        //List<?> dataSourceList = src;
 
-		JRDataSource dataSource = new JRBeanCollectionDataSource(src);
+        JRDataSource dataSource = new JRBeanCollectionDataSource(src);
 
-		Map<String, Object> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
 
-		try {
-			byte[] result = getPdfBytes("sample4", params, dataSource);
-			return Response.ok(result)
-					// .header("Content-Disposition", "attachment; filename=" +
-					// URLEncoder.encode("test.pdf", "utf-8"))
-					.build();
-		} catch (JRException e) {
-			return Response.serverError().build();
-		}
-	}
+        try {
+            byte[] result = getPdfBytes("sample4", params, dataSource);
+            return Response.ok(result)
+                    // .header("Content-Disposition", "attachment; filename=" +
+                    // URLEncoder.encode("test.pdf", "utf-8"))
+                    .build();
+        } catch (JRException e) {
+            return Response.serverError().build();
+        }
+    }
 
-	/**
-	 * PDF(オブジェクト関連付け+QRコード)の表示
-	 * @return
-	 */
-	@GET
-	@Path("pdf5")
-	@Produces("application/pdf")
-	public Response pdf5() {
+    /**
+     * PDF(オブジェクト関連付け+QRコード)の表示
+     * @return
+     */
+    @GET
+    @Path("pdf5")
+    @Produces("application/pdf")
+    public Response pdf5() {
 
-		List<TestData> src = new ArrayList<>();
-		src.add(new TestData("name20", 20));
-		src.add(new TestData("name21", 21));
-		src.add(new TestData("name22", 22));
-		src.add(new TestData("name23", 23));
-		src.add(new TestData("name24", 24));
-		src.add(new TestData("name25", 25));
-		//List<?> dataSourceList = src;
+        List<TestData> src = new ArrayList<>();
+        src.add(new TestData("name20", 20));
+        src.add(new TestData("name21", 21));
+        src.add(new TestData("name22", 22));
+        src.add(new TestData("name23", 23));
+        src.add(new TestData("name24", 24));
+        src.add(new TestData("name25", 25));
+        //List<?> dataSourceList = src;
 
-		JRDataSource dataSource = new JRBeanCollectionDataSource(src);
+        JRDataSource dataSource = new JRBeanCollectionDataSource(src);
 
-		Map<String, Object> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
 
-		try {
-			byte[] result = getPdfBytes("sample5", params, dataSource);
-			return Response.ok(result)
-					// .header("Content-Disposition", "attachment; filename=" +
-					// URLEncoder.encode("test.pdf", "utf-8"))
-					.build();
-		} catch (JRException e) {
-			return Response.serverError().build();
-		}
-	}
+        try {
+            byte[] result = getPdfBytes("sample5", params, dataSource);
+            return Response.ok(result)
+                    // .header("Content-Disposition", "attachment; filename=" +
+                    // URLEncoder.encode("test.pdf", "utf-8"))
+                    .build();
+        } catch (JRException e) {
+            return Response.serverError().build();
+        }
+    }
 
-	/**
-	 * PDFデータの取得
-	 *
-	 * @param reportName レポート名
-	 * @param params パラメータ
-	 * @param dataSource データソース
-	 * @return PDFデータ
-	 * @throws JRException 実行時の例外エラー
-	 */
-	private byte[] getPdfBytes(String reportName, Map<String, Object> params, JRDataSource dataSource)
-			throws JRException {
-		String filePath = getResourcePath(reportName + ".jasper");
+    /**
+     * PDFデータの取得
+     *
+     * @param reportName レポート名
+     * @param params パラメータ
+     * @param dataSource データソース
+     * @return PDFデータ
+     * @throws JRException 実行時の例外エラー
+     */
+    private byte[] getPdfBytes(String reportName, Map<String, Object> params, JRDataSource dataSource)
+            throws JRException {
+        String filePath = getResourcePath(reportName + ".jasper");
 
-		if (filePath != null) {
-			return JasperRunManager.runReportToPdf(filePath, params, dataSource);
-		}
+        if (filePath != null) {
+            return JasperRunManager.runReportToPdf(filePath, params, dataSource);
+        }
 
-		filePath = getResourcePath(reportName + ".jrxml");
-		JasperReport report = JasperCompileManager.compileReport(filePath);
-		return JasperRunManager.runReportToPdf(report, params, dataSource);
-	}
+        filePath = getResourcePath(reportName + ".jrxml");
+        JasperReport report = JasperCompileManager.compileReport(filePath);
+        return JasperRunManager.runReportToPdf(report, params, dataSource);
+    }
 
-	/**
-	 * リソースのパスを取得する
-	 *
-	 * @param fileName リソースファイル名
-	 * @return リソースファイルのパス
-	 */
-	private String getResourcePath(String fileName) {
-		URL fileUrl = DownloadResource.class.getClassLoader().getResource(fileName);
-		if (fileUrl == null) {
-			return null;
-		}
-		return fileUrl.getPath();
-	}
+    /**
+     * リソースのパスを取得する
+     *
+     * @param fileName リソースファイル名
+     * @return リソースファイルのパス
+     */
+    private String getResourcePath(String fileName) {
+        URL fileUrl = DownloadResource.class.getClassLoader().getResource(fileName);
+        if (fileUrl == null) {
+            return null;
+        }
+        return fileUrl.getPath();
+    }
 }
